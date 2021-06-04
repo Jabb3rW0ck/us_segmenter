ffb7cc8724c8ee37342f21b8123f5609915e1af2
Jabb3rW0ck <64436777+Jabb3rW0ck@users.noreply.github.com>
2021-06-04 14:59:42 -0400

    first commit, establish video splitter and segmenter files

---
+++ segmenter.py
@@ -0,0 +1,11 @@
+import os
+import opencv
+from video_splitter import split_video
+
+def create_masks(img_folder_path, seg_folder_path, seg_frame_path):
+    """
+    Creates masks based on the segmented frame found in seg_frame_path.
+    """
+    pass
+
+
---
+++ video_splitter.py
@@ -0,0 +1,14 @@
+import os
+import opencv
+
+def split_video(path):
+    """
+    Splits the video at path :path: into images and places them into a folder with the video's filename. 
+    All images are named with the video filename, an underscore, and then the number of the frame.
+    """
+    pass
+
+if __name__ == "__main__":
+    filename = "pleural_effusion_clip.mp4"
+    split_video(filename)
+    
